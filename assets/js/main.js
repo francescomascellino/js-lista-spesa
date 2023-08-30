1c486326b326abebfc7e3ab1f038af2a6786f8ba
francescomascellino <franesco.mascellino@gmail.com>
2023-08-30 14:34:00 +0200

    add method 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,15 @@ Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ci
 Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while 
 */
 
+/* Global Variables */
+
 let shoppingList = ["Pane", "Acqua", "Uova", "Olio"]
 
 let intemCounter = 0;
 
 console.log("Ecco la lista della spesa", shoppingList);
+
+/* METHOD 1 - getElementById and Template Literal */
 
 const shoppingListNode = document.getElementById("shoppingListNode");
 
@@ -22,3 +26,24 @@ while (intemCounter < shoppingList.length) {
     intemCounter++
 
 }
+
+/* METHOD 2 - querySelector */
+
+const shoppingListNode2 = document.querySelector("ul")
+
+console.log("shoppiong list node 2 = ", shoppingListNode2);
+
+while (intemCounter < shoppingList.length) {
+
+    const listElement2 = document.createElement("li");
+
+    listElement2.innerText = shoppingList[intemCounter];
+
+    console.log("list Element 2 with text = ", listElement2);
+
+    shoppingListNode2.insertAdjacentElement("beforeend", listElement2)
+
+    intemCounter++
+
+}
+
